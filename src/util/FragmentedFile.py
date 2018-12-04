06abb0540e51fc7ec9482ef2668896b1bd441e18
Yuchen Li <ychnlgy@gmail.com>
2018-12-04 14:09:25 -0500

    Uploaded spectrogram saving

--- src/util/FragmentedFile.py
+++ src/util/FragmentedFile.py
@@ -22,14 +22,14 @@ class FragmentedFile:
     def dump(self, n, it):
         self.n = n
         with open(self.f, "wb") as f:
-            numpy.save(f, FragmentedFile.KEY + str(n))
+            numpy.save(f, [FragmentedFile.KEY + str(n)])
             counter = tqdm.tqdm(range(1, n+1), desc="Dump >> %s" % self.f, ncols=80)
             for i, data in zip(counter, it):
                 numpy.save(f, data)
         assert i == n
     
     def _loadkey(self, f):
-        key = numpy.load(f)
+        key = numpy.load(f)[0]
         assert key.startswith(FragmentedFile.KEY)
         num = key.rstrip(FragmentedFile.KEY)
         assert num.isdigit()
