f70b9ef3adc75d8970d6678886c08ebc8c6eb7b8
Yuchen Li <ychnlgy@gmail.com>
2018-12-05 15:42:21 -0500

    Added full training

--- src/train.py
+++ src/train.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 
-import numpy, torch
+import numpy, torch, tqdm
 
 import util, torchmods, load
 
 from SeqToSeq import SeqToSeq
 
 def load_dataset(datadir, batchsize=300, validsplit=0.2):
-    X, Y = [], []
-    for x, y in load.load(datadir):
+    X, Y, L = [], [], []
+    for x, y, l in load.load(datadir):
         X.append(x)
         Y.append(y)
     
     X = numpy.array(X)
     Y = numpy.array(Y)
+    L = numpy.array(L)
     N = len(X)
     
     idx = numpy.arange(N)
@@ -22,8 +23,8 @@ def load_dataset(datadir, batchsize=300, validsplit=0.2):
     tidx = idx[p:]
     vidx = idx[:p]
     
-    dataloader = make_dataloader(X[tidx], Y[tidx], batchsize)
-    testloader = make_dataloader(X[vidx], Y[vidx], batchsize)
+    dataloader = make_dataloader(X[tidx], Y[tidx], L[tidx], batchsize)
+    testloader = make_dataloader(X[vidx], Y[vidx], L[vidx], batchsize)
     return dataloader, testloader
 
 def make_dataloader(X, Y, batchsize):
@@ -36,8 +37,11 @@ def make_dataloader(X, Y, batchsize):
 @util.main(__name__)
 def main(
     modelf,
-    datadir
+    datadir,
+    epochs
 ):
+    
+    epochs = int(epochs)
     
     device = torchmods.DEVICE
     
@@ -45,4 +49,63 @@ def main(
     
     dataloader, testloader = load_dataset(datadir)
     
-    print(len(dataloader), len(testloader))
+    lossf = torch.nn.CrossEntropyLoss()
+    optim = torch.optim.Adam(model.parameters())
+    sched = torch.optim.lr_scheduler.ReduceLROnPlateau(optim)
+    
+    lowest = float("inf")
+    
+    for epoch in range(epochs):
+        
+        model.train()
+        
+        e = n = 0.0
+        
+        bar = tqdm.tqdm(dataloader, ncols=80)
+        
+        for X, y, labels in bar:
+            X = X.to(device)
+            y = y.to(device)
+            l = l.to(device)
+            
+            yh = model(X, l)
+            N, seq, C = yh.size()
+            yh = yh.view(N*seq, C)
+            loss = lossf(yh, y.view(-1))
+            
+            e += loss.item()
+            n += 1.0
+            
+            optim.zero_grad()
+            loss.backward()
+            optim.step()
+            
+            bar.set_description("[E%d] %.3f" % (epoch, e/n))
+        
+        model.eval()
+        with torch.no_grad():
+            
+            e = n = 0.0
+            
+            bar = tqdm.tqdm(testloader, ncols=80)
+            
+            for X, y, labels in bar:
+                X = X.to(device)
+                y = y.to(device)
+                l = l.to(device)
+                
+                yh = model(X, l)
+                loss = lossf(yh.view(-1, yh.size(-1)), y.view(-1))
+                
+                e += loss.item()
+                n += 1
+                
+                bar.set_description(" >> %.3f" % (e/n))
+            
+            verr = e/n
+            sched.step(verr)
+            
+            if verr < lowest:
+                lowest = verr
+                model.save(modelf)
+            
